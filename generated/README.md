a945ac9ce1dc156103997dc8ec552b7fb661fa64
Michael Klein <Michaelklein1997@gmail.com>
2022-01-22 16:35:34 -0600

    added all necessary questions to prompt

--- generated/README.md
+++ generated/README.md
@@ -1,23 +1,23 @@
-# 
+# README.md Generator
 
   ## Table of Contents
   
-  [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+  [![License: CC BY 4.0](https://img.shields.io/badge/License-CC_BY_4.0-lightgrey.svg)](https://creativecommons.org/licenses/by/4.0/)
 
   ## Description
-  
+  undefined
   
   ## Installation
-  
+  true
   
   ## Usage
-  
+  node index.js
   
   ## License
-  This project is licensed under the terms of [MIT](https://opensource.org/licenses/MIT).
+  This project is licensed under the terms of [Creative Commons Attribution 4.0](https://creativecommons.org/licenses/by/4.0/).
   
   ## Contributing
-  
+  You may contribute to this project via issues and pull requests on GitHub
   
   ## Tests
-  
+  IDK what this means
--- index.js
+++ index.js
@@ -11,29 +11,52 @@ const questions = [
         name: 'title'
     },
     {
-        type: 'input',
-        message: 'Input project iescription',
-        name: 'description'
+        type: 'checkbox',
+        message: 'Which of the following sections would you like in your README.md?',
+        name: 'contents',
+        choices: ['Description', 'Installation','Usage', 'License','Contribution','Tests','Questions'],
     },
     {
         type: 'input',
-        message: 'Input installation instructions',
-        name: 'installation'
+        message: 'What is your project? (Complete Statement): [Title] is a ',
+        name: 'whatDesc',
+        when: (answers) => answers.contents.includes('Description'),
     },
     {
         type: 'input',
-        message: 'Input usage information',
-        name: 'usage'
+        message: 'Why did you create your project? (Complete Statement): [Title] was created to ',
+        name: 'whyDesc',
+        when: (answers) => answers.contents.includes('Description'),
+    },
+    {
+        type: 'confirm',
+        message: 'Do any packages need to be installed for you application?',
+        name: 'installation',
+        when: (answers) => answers.contents.includes('Installation'),
     },
     {
         type: 'input',
-        message: 'Input contribution guidelines',
-        name: 'contribution'
+        message: 'What is the name of the package?',
+        name: 'packageName',
+        when: (answers) => answers.installation === true,
     },
     {
         type: 'input',
-        message: 'Input test instructions',
-        name: 'test'
+        message: "Enter that package's URL",
+        name: 'packageURL',
+        when: (answers) => answers.installation === true,
+    },
+    {
+        type: 'input',
+        message: 'What code snippit is needed to install that package?',
+        name: 'packageInstallCode',
+        when: (answers) => answers.installation === true,
+    },
+    {
+        type: 'input',
+        message: 'What code is needed to invoke [Title]?',
+        name: 'usage',
+        when: (answers) => answers.contents.includes('Usage')
     },
     {
         type: 'list',
@@ -41,12 +64,48 @@ const questions = [
         name: 'license',
         choices: ['No license','MIT','ISC','Creative Commons Attribution 4.0', 'GNU General Public License v3.0']
     },
+    {
+        type: 'confirm',
+        message: "Because you have opted for a license, do you agree to Contributer Covenent's Code of Conduct?  For more information visit https://www.contributor-covenant.org/",
+        name: 'agreementCOC',
+        when: (answers) => answers.license != 'No license',
+    },
+    {
+        type: 'input',
+        message: 'Input contribution guidelines',
+        name: 'contribution',
+        when: (answers) => answers.contents.includes('Contribution'),
+    },
+    {
+        type: 'input',
+        message: 'Input test instructions',
+        name: 'test'
+    },
+    {
+        type: 'checkbox',
+        message: 'Which platforms would you like for people to reach out to you with questions?',
+        name: 'platform',
+        choices: ['Email','GitHub'],
+    },
+    {
+        type: 'input',
+        message: 'Enter your Email',
+        name: 'email',
+        when: (answers) => answers.platform.includes('Email'),
+    },
+    {
+        type: 'input',
+        message: 'Enter your GitHub username',
+        name: 'github',
+        when: (answers) => answers.platform.includes('GitHub'),
+    },
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(data) {
-    console.log(data)
-    fs.writeFile('./generated/README.md', generateMarkdown(data), (err) => {
+function writeToFile(answers) {
+    console.log(answers)
+    console.log(answers.contents)
+    fs.writeFile('./generated/README.md', generateMarkdown(answers), (err) => {
         err ? console.log(err) : console.log('README.md generated!')
     })
 }
